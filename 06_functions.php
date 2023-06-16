5c4c183b525fd9c2b1121b7be8e1e482fd78f0b4
_Tess Cee <stacymonica543@gmail.com>
2023-06-16 11:34:06 +0300

    notes

--- 06_functions.php
+++ 06_functions.php
@@ -4,7 +4,9 @@
 /*
   Functions are reusable blocks of code that we can call to perform a specific task.
   We can pass values into functions to change their behavior. Functions have their own local scope as opposed to global scope
-
+types
+built-in
+user-defined-
 
 /*
 ** Function Syntax
@@ -12,3 +14,19 @@
     // code to be executed
   }
 */
+function hellomessage(){
+echo "this is a message<br>";
+}
+hellomessage();
+
+function addingfunction($num1, $num2){
+  $sum=$num1 + $num2;
+  echo "print $sum <br>";
+}
+addingfunction(50,20);
+
+function myName($fname,$lname){
+  $myNames=$fname.$lname;
+  echo " print $myNames";
+}
+myName("stacy", "monica");
